9f6a2f342beaa56db7f3ca6b7b363707c0146944
Shafwan Ahmed Dehan <88941317+ShafwanAhmedDehan@users.noreply.github.com>
2023-09-30 13:21:53 +0600

    login complete by DH

--- jwt-auth/routes/api.php
+++ jwt-auth/routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SystemLogin\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +17,3 @@ use App\Http\Controllers\SystemLogin\LoginController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route :: post("Login", [LoginController::class, 'GetLoginInfo']);

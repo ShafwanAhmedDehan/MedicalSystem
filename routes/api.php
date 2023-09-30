05da6cba4e51693296d5ed6259cc72638e5c5ccd
Shafwan Ahmed Dehan <88941317+ShafwanAhmedDehan@users.noreply.github.com>
2023-09-30 13:43:08 +0600

    changeby DH

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route :: post("Login", [LoginController::class, 'GetLoginInfo']);
+Route :: post("login", [LoginController::class, 'GetLoginInfo']);
